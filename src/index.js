0dbf4f3e7c58b9b4cbc7a8139f0ce945612fd56b
David Dinh <dmdinh22@gmail.com>
2018-05-11 22:31:00 -0700

    bringing in mysql and bodyparser

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
 const express = require('express');
+mysql = require('mysql');
+bodyParser = require('body-parser');
+
+// DB info
+const config = {
+    host: 'localhost',
+    userName: 'root',
+    password: 'Password1',
+    database: 'sampleapi'
+};
+
+// configure db connection
+const pool = mysql.createPool({
+    host: config.host,
+    userName: config.userName,
+    password: config.password,
+    database: config.database
+});
 
 const portVal = 9000;
 
 const api = express();
 // setting port
 api.set('port', portVal);
+
+// add support for JSON parsing
+api.use(bodyParser.urlencoded({ extended: true }));
+api.use(bodyParser.json());
+
+// POST
+
+// GET
 
 // GET endpoint to sennd response to web client
 api.get('/', (req, res) => {
